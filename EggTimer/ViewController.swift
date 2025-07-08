8cfb33bb45912902668f0d673d16288f46851cfd
Lakshaya Singh Tanwar <tanwarlakshay21@gmail.com>
2025-07-08 20:27:34 +0530

    Initial commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Lakshaya Singh Tanwar on 01/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EggTimerTests/EggTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerTests.swift
+//  EggTimerTests
+//
+//  Created by Lakshaya Singh Tanwar on 01/07/25.
+//
+
+import Testing
+@testable import EggTimer
+
+struct EggTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
